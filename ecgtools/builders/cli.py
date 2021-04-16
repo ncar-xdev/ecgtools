e703af7652c4095817ec061f2ee76d2e62ccb884
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-04-16 15:13:37 -0600

    Add new options

--- ecgtools/builders/cli.py
+++ ecgtools/builders/cli.py
@@ -1,3 +1,5 @@
+import pathlib
+from enum import Enum
 from typing import List
 
 import typer
@@ -11,9 +13,21 @@ from .cesm import symle_parser
 
 app = typer.Typer(help='ESM Catalog Generation CLI')
 
+parsers = {'cesm2_symle': symle_parser}
+
+
+class Collection(Enum):
+    cesm2_symle = 'cesm2-symle'
+
+
+class DataFormat(Enum):
+    netcdf = 'netcdf'
+    zarr = 'zarr'
+
 
 @app.command()
-def symle(
+def build(
+    collection: Collection = typer.Argument(..., help='Collection name', case_sensitive=False),
     root_path: str = typer.Argument(..., help='Path of root directory'),
     extension: str = typer.Option('*.nc', help='File extension', show_default=True),
     depth: int = typer.Option(
@@ -30,24 +44,42 @@ def symle(
         25, help='Number of tasks to batch in a single `dask.delayed` call'
     ),
     jobs: int = typer.Option(4, help='Number of batch jobs to submit'),
+    path_column: str = typer.Option(
+        'path', help='The name of the column containing the path to the asset.'
+    ),
+    variable_column: str = typer.Option(
+        'variable', help='Name of the attribute column in catalog that contains the variable name'
+    ),
+    data_format: DataFormat = typer.Option('netcdf', help='The data format. '),
+    catalog_name: str = typer.Option(None, help='Catalog Name'),
+    output_path: pathlib.Path = typer.Option(
+        '.', help='Output directory path', exists=True, dir_okay=True
+    ),
 ):
 
     cluster = NCARCluster(cores=8, processes=8, memory='80GB')
     cluster.scale(jobs=jobs)
     client = Client(cluster)
 
-    with console.status('[bold green]Building catalog...'):
+    with console.status(f'[bold green]Building catalog for {collection.value}...'):
+        if catalog_name is None:
+            catalog_file = f'{collection.value}.csv'
+        else:
+            catalog_file = f'{catalog_name}.csv'
+        catalog_file = output_path / catalog_file
         _ = (
             Builder(
                 root_path,
                 extension=extension,
                 depth=depth,
-                parser=symle_parser,
+                parser=parsers[collection.value],
                 exclude_patterns=exclude_patterns,
                 nbatches=nbatches,
             )
-            .build(path_column='path', variable_column='variable')
-            .save(catalog_file='campaign-cesm2-symle.csv')
+            .build(
+                path_column=path_column, variable_column=variable_column, data_format=data_format
+            )
+            .save(catalog_file=catalog_file)
         )
     cluster.close()
     client.close()
@@ -60,12 +92,3 @@ def test():
 
 def main():
     typer.run(app())
-
-
-#  root_path: str,
-#     extension: str = '*.nc',
-#     depth: int = 0,
-#     exclude_patterns: list = None,
-#     parser: <built-in function callable> = None,
-#     lazy: bool = True,
-#     nbatches: int = 25,
