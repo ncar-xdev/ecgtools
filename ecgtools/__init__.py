864e95b16b98c5c9ddea8e6ab00dbba84bac1982
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-05-27 15:16:08 -0600

    Add save method to main Builder class (#23)

--- ecgtools/__init__.py
+++ ecgtools/__init__.py
@@ -3,7 +3,7 @@
 """Top-level module for ecgtools ."""
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .builder import INVALID_ASSET, TRACEBACK, Builder
+from .builder import Builder
 
 try:
     __version__ = get_distribution(__name__).version
--- ecgtools/builder.py
+++ ecgtools/builder.py
@@ -1,5 +1,8 @@
+import datetime
+import enum
 import fnmatch
 import itertools
+import json
 import pathlib
 import typing
 
@@ -7,8 +10,39 @@ import joblib
 import pandas as pd
 import pydantic
 
-INVALID_ASSET = 'INVALID_ASSET'
-TRACEBACK = 'TRACEBACK'
+
+class DataFormatEnum(str, enum.Enum):
+    netcdf = 'netcdf'
+    zarr = 'zarr'
+
+
+class Attribute(pydantic.BaseModel):
+    column_name: str
+    vocabulary: str = None
+
+
+class Assets(pydantic.BaseModel):
+    column_name: str
+    format: DataFormatEnum
+
+
+class Aggregation(pydantic.BaseModel):
+    type: str
+    attribute_name: str
+    options: typing.Dict[str, typing.Any]
+
+
+class ESMCollection(pydantic.BaseModel):
+    catalog_file: typing.Union[str, pathlib.Path, pydantic.AnyUrl]
+    attributes: typing.List[Attribute]
+    assets: Assets
+    variable_column: str
+    groupby_attrs: typing.List[str] = None
+    aggregations: typing.List[Aggregation] = None
+    esmcat_version: str = '0.0.1'
+    id: str = None
+    description: str = None
+    last_updated: typing.Union[datetime.datetime, datetime.date] = None
 
 
 @pydantic.dataclasses.dataclass
@@ -32,12 +66,14 @@ class Builder:
 
     """
 
-    root_path: pydantic.types.DirectoryPath
-    extension: str = '*.nc'
+    root_path: pydantic.DirectoryPath
+    extension: str = '.nc'
     depth: int = 0
     exclude_patterns: typing.List[str] = None
     parsing_func: typing.Callable = None
     njobs: int = -1
+    INVALID_ASSET: typing.ClassVar[str] = 'INVALID_ASSET'
+    TRACEBACK: typing.ClassVar[str] = 'TRACEBACK'
 
     def __post_init_post_parse__(self):
         self.df = pd.DataFrame()
@@ -47,6 +83,14 @@ class Builder:
         self.entries = None
 
     def get_directories(self):
+        """
+        Walk `root_path`'s subdirectories and returns a list of directories
+        up to the specified depth from `root_path`.
+
+        Returns
+        -------
+        `ecgtools.Builder`
+        """
         pattern = '*/' * (self.depth + 1)
         dirs = [x for x in self.root_path.glob(pattern) if x.is_dir()]
         if not dirs:
@@ -64,7 +108,7 @@ class Builder:
             )
 
         def _glob_dir(directory, extension):
-            return list(directory.rglob(f'{extension}'))
+            return list(directory.rglob(f'*{extension}'))
 
         filelist = joblib.Parallel(n_jobs=self.njobs, verbose=5)(
             joblib.delayed(_glob_dir)(directory, self.extension) for directory in self.dirs
@@ -87,28 +131,101 @@ class Builder:
         return self
 
     def clean_dataframe(self):
-        if INVALID_ASSET in self.df.columns:
-            invalid_assets = self.df[self.df[INVALID_ASSET].notnull()][[INVALID_ASSET, TRACEBACK]]
-            df = self.df[self.df[INVALID_ASSET].isnull()].drop(columns=[INVALID_ASSET, TRACEBACK])
+        if self.INVALID_ASSET in self.df.columns:
+            invalid_assets = self.df[self.df[self.INVALID_ASSET].notnull()][
+                [self.INVALID_ASSET, self.TRACEBACK]
+            ]
+            df = self.df[self.df[self.INVALID_ASSET].isnull()].drop(
+                columns=[self.INVALID_ASSET, self.TRACEBACK]
+            )
             self.invalid_assets = invalid_assets
             self.df = df
         return self
 
+    def build(self):
+        self.get_directories().get_filelist().parse().clean_dataframe()
+        return self
+
     def save(
         self,
-        catalog_file: typing.Union[pathlib.Path, str],
+        catalog_file: typing.Union[str, pathlib.Path, pydantic.AnyUrl],
+        path_column: str,
+        variable_column: str,
+        data_format: DataFormatEnum,
+        groupby_attrs: typing.List[str] = None,
+        aggregations: typing.List[Aggregation] = None,
+        esmcat_version: str = '0.0.1',
+        id: str = None,
+        description: str = None,
+        last_updated: typing.Union[datetime.datetime, datetime.date] = None,
         **kwargs,
     ):
+        """Persist catalog contents to files.
+
+        Parameters
+        ----------
+        catalog_file : str
+           Path to a the CSV file in which catalog contents will be persisted.
+        path_column : str
+           The name of the column containing the path to the asset.
+           Must be in the header of the CSV file.
+        variable_column : str
+            Name of the attribute column in csv file that contains the variable name.
+        data_format : str
+            The data format. Valid values are netcdf and zarr.
+        aggregations : List[dict]
+            List of aggregations to apply to query results, default None
+        esmcat_version : str
+            The ESM Catalog version the collection implements, default None
+        id : str
+            Identifier for the collection, default None
+        description : str
+            Detailed multi-line description to fully explain the collection,
+            default None
+        kwargs : Additional keyword arguments are passed through to the
+                 :py:class:`~pandas.DataFrame.to_csv` method.
+
+        Returns
+        -------
+        `ecgtools.Builder`
+
+        Notes
+        -----
+        See https://github.com/NCAR/esm-collection-spec/blob/master/collection-spec/collection-spec.md
+        for mor
+
+        """
+        last_updated = last_updated or datetime.datetime.now().utcnow().strftime(
+            '%Y-%m-%dT%H:%M:%SZ'
+        )
+
+        for col in {variable_column, path_column}.union(set(groupby_attrs or [])):
+            assert col in self.df.columns, f'{col} must be a column in the dataframe.'
+
+        attributes = [Attribute(column_name=column, vocabulary='') for column in self.df.columns]
+
+        esmcol_data = ESMCollection(
+            catalog_file=catalog_file,
+            variable_column=variable_column,
+            attributes=attributes,
+            assets=Assets(column_name=path_column, format=data_format),
+            groupby_attrs=groupby_attrs,
+            aggregations=aggregations,
+            esmcat_version=esmcat_version,
+            id=id,
+            description=description,
+            last_updated=last_updated,
+        )
+        esmcol_data = json.loads(esmcol_data.json())
         catalog_file = pathlib.Path(catalog_file)
         index = kwargs.pop('index') if 'index' in kwargs else False
         self.df.to_csv(catalog_file, index=index, **kwargs)
         if not self.invalid_assets.empty:
             invalid_assets_report_file = (
-                catalog_file.parent / f'invalid_assets_{catalog_file.parts[-1]}'
+                catalog_file.parent / f'invalid_assets_{catalog_file.stem}.csv'
             )
             self.invalid_assets.to_csv(invalid_assets_report_file, index=False)
-        print(f'Saved catalog location: {catalog_file}')
-
-    def build(self):
-        self.get_directories().get_filelist().parse().clean_dataframe()
-        return self
+        json_path = catalog_file.parent / f'{catalog_file.stem}.json'
+        with open(json_path, mode='w') as outfile:
+            json.dump(esmcol_data, outfile, indent=2)
+        print(f'Saved catalog location: {json_path} and {catalog_file}')
