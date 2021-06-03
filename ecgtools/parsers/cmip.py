978bf12de3e9526ca17486021a0ad311140c8d3c
Max Grover <mgroverwx@gmail.com>
2021-06-03 14:15:26 -0500

    Update ecgtools/parsers/cmip.py
    Co-authored-by: Anderson Banihirwe <axbanihirwe@ualr.edu>

--- ecgtools/parsers/cmip.py
+++ ecgtools/parsers/cmip.py
@@ -74,7 +74,7 @@ def parse_cmip6(file):
             attributes['init_year'] = init_year
             attributes['start_time'] = start_time
             attributes['end_time'] = end_time
-            if (start_time is None) & (end_time is None):
+            if not (start_time and end_time):
                 attributes['time_range'] = None
             else:
                 attributes['time_range'] = f'{start_time}-{end_time}'
