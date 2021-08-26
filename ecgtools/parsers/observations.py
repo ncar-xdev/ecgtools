8acbeb9f9e48144a983767c6d2b1c8f57c8d2825
Max Grover <mgroverwx@gmail.com>
2021-08-26 14:58:56 -0600

    add amwg obs parser

---
+++ ecgtools/parsers/observations.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import pathlib
+import traceback
+import warnings
+
+import xarray as xr
+
+from ..builder import INVALID_ASSET, TRACEBACK
+
+
+def parse_amwg_obs(file):
+    """Atmospheric observational data stored in """
+    file = pathlib.Path(file)
+    info = {}
+
+    try:
+        stem = file.stem
+        split = stem.split('_')
+        source = split[0]
+        temporal = split[-2]
+        if len(split[-2]) == 2:
+            month_number = int(temporal)
+            time_period = 'monthly'
+            temporal = datetime(2020, month_number, 1).strftime('%b').upper()
+        else:
+            time_period = 'seasonal'
+        
+        with xr.open_dataset(file, chunks={}, decode_times=False) as ds:
+            for var in ds:
+                da = ds[var]
+                units = da.units
+                long_name = da.long_name
+                info = {'source':source,
+                        'temporal':temporal,
+                        'time_period':time_period,
+                        'variable':var,
+                        'units':units,
+                        'long_name':long_name,
+                        'path':str(path)}
+
+            
+        return info
+
+    except Exception:
+        return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
