f48c32bc0bc572837c9c193b538c44f5aea1c43b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-01-07 13:14:16 -0700

    Set storage_options

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,29 +20,34 @@ def test_directory(path, depth):
 
 
 @pytest.mark.parametrize(
-    'paths, depth',
+    'paths, depth, storage_options',
     [
-        ([str(sample_data_dir / 'cmip' / 'CMIP6' / 'CMIP' / 'BCC')], 2),
-        (['s3://ncar-cesm-lens/atm/monthly'], 1),
+        ([str(sample_data_dir / 'cmip' / 'CMIP6' / 'CMIP' / 'BCC')], 2, {}),
+        (['s3://ncar-cesm-lens/atm/monthly'], 1, {'anon': True}),
     ],
 )
-def test_builder_init(paths, depth):
-    builder = Builder(paths=paths, depth=depth)
+def test_builder_init(paths, depth, storage_options):
+    builder = Builder(paths=paths, depth=depth, storage_options=storage_options)
     builder.get_directories()
     assert isinstance(builder.directories, list)
     assert len(builder.directories) > 0
 
 
 @pytest.mark.parametrize(
-    'paths, depth, pattern',
+    'paths, depth, pattern, storage_options',
     [
-        ([str(sample_data_dir / 'cmip' / 'CMIP6' / 'CMIP' / 'BCC')], 2, '**.nc'),
-        (['s3://ncar-cesm-lens/atm/'], 1, '*.zarr'),
-        (['gs://cmip6/CMIP6/CMIP/BCC/BCC-ESM1/piControl/r1i1p1f1/SImon'], 1, '*/*v????????'),
+        ([str(sample_data_dir / 'cmip' / 'CMIP6' / 'CMIP' / 'BCC')], 2, '**.nc', {}),
+        (['s3://ncar-cesm-lens/atm/'], 1, '*.zarr', {'anon': True}),
+        (
+            ['gs://cmip6/CMIP6/CMIP/BCC/BCC-ESM1/piControl/r1i1p1f1/SImon'],
+            1,
+            '*/*v????????',
+            {'token': 'anon'},
+        ),
     ],
 )
-def test_builder_get_asset_list(paths, depth, pattern):
-    builder = Builder(paths=paths, depth=depth)
+def test_builder_get_asset_list(paths, depth, pattern, storage_options):
+    builder = Builder(paths=paths, depth=depth, storage_options=storage_options)
     builder.get_asset_list(pattern=pattern)
     assert isinstance(builder.asset_list, list)
     assert len(builder.asset_list) > 0
