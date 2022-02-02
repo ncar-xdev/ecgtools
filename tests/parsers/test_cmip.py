408666cd20eb8bfaecccb70b17af7736809a3183
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-02-01 21:23:10 -0700

    Add tests for CMIP6 parser

---
+++ tests/parsers/test_cmip.py
@@ -0,0 +1,20 @@
+import pytest
+
+from ecgtools.parsers.cmip import parse_cmip6
+
+
+@pytest.mark.parametrize(
+    'file_path',
+    [
+        'cmip/CMIP6/CMIP/BCC/BCC-ESM1/piControl/r1i1p1f1/Amon/tasmax/gn/v20181214/tasmax/tasmax_Amon_BCC-ESM1_piControl_r1i1p1f1_gn_185001-230012.nc'
+    ],
+)
+def test_parse_cmip6(sample_data_directory, file_path):
+    path = sample_data_directory / file_path
+    entry = parse_cmip6(path)
+    assert {'activity_id', 'variable_id', 'table_id'}.issubset(set(list(entry.keys())))
+    assert entry['experiment_id'] == 'piControl'
+    assert entry['member_id'] == 'r1i1p1f1'
+    assert entry['grid_label'] == 'gn'
+    assert entry['table_id'] == 'Amon'
+    assert entry['variable_id'] == 'tasmax'
