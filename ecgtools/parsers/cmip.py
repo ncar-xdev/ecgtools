9d77613f3a4ef848ef73db22a64b9e9ebbdfc833
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-03-09 08:25:16 -0700

    Add new CMIP6 filepath parser

--- ecgtools/parsers/cmip.py
+++ ecgtools/parsers/cmip.py
@@ -1,4 +1,4 @@
-import pathlib
+import os
 import traceback
 
 import cf_xarray  # noqa
@@ -83,7 +83,7 @@ def parse_cmip6(file):
         return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
 
 
-def parse_cmip6_using_directories(file):
+def parse_cmip6_filename(filepath: str) -> dict[str, str]:
     """
     Extract attributes of a file using information from CMI6 DRS.
     References
@@ -105,7 +105,7 @@ def parse_cmip6_using_directories(file):
     Example when there is no sub-experiment: tas_Amon_GFDL-CM4_historical_r1i1p1f1_gn_196001-199912.nc
     Example with a sub-experiment: pr_day_CNRM-CM6-1_dcppA-hindcast_s1960-r2i1p1f1_gn_198001-198412.nc
     """
-    basename = pathlib.Path(file).name
+    basename = os.path.basename(filepath)
     filename_template = '{variable_id}_{table_id}_{source_id}_{experiment_id}_{member_id}_{grid_label}_{time_range}.nc'
 
     gridspec_template = (
@@ -114,7 +114,7 @@ def parse_cmip6_using_directories(file):
     templates = [filename_template, gridspec_template]
     fileparts = reverse_filename_format(basename, templates=templates)
     try:
-        parent = str(pathlib.Path(file).parent)
+        parent = os.path.dirname(filepath).strip('/')
         parent_split = parent.split(f"/{fileparts['source_id']}/")
         part_1 = parent_split[0].strip('/').split('/')
         grid_label = parent.split(f"/{fileparts['variable_id']}/")[1].strip('/').split('/')[0]
@@ -124,7 +124,7 @@ def parse_cmip6_using_directories(file):
         version_regex = r'v\d{4}\d{2}\d{2}|v\d{1}'
         version = extract_attr_with_regex(parent, regex=version_regex) or 'v0'
         fileparts['version'] = version
-        fileparts['path'] = file
+        fileparts['path'] = filepath
         if fileparts['member_id'].startswith('s'):
             fileparts['dcpp_init_year'] = float(fileparts['member_id'].split('-')[0][1:])
             fileparts['member_id'] = fileparts['member_id'].split('-')[-1]
@@ -132,45 +132,6 @@ def parse_cmip6_using_directories(file):
             fileparts['dcpp_init_year'] = np.nan
 
     except Exception:
-        return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
-
-    return fileparts
-
-
-def parse_cmip5_using_directories(file):
-    """Extract attributes of a file using information from CMIP5 DRS.
-    Notes
-    -----
-    Reference:
-    - CMIP5 DRS: https://pcmdi.llnl.gov/mips/cmip5/docs/cmip5_data_reference_syntax.pdf?id=27
-    """
-
-    freq_regex = r'/3hr/|/6hr/|/day/|/fx/|/mon/|/monClim/|/subhr/|/yr/'
-    realm_regex = r'aerosol|atmos|land|landIce|ocean|ocnBgchem|seaIce'
-    version_regex = r'v\d{4}\d{2}\d{2}|v\d{1}'
-
-    file_basename = str(pathlib.Path(file).name)
-
-    filename_template = (
-        '{variable}_{mip_table}_{model}_{experiment}_{ensemble_member}_{temporal_subset}.nc'
-    )
-    gridspec_template = '{variable}_{mip_table}_{model}_{experiment}_{ensemble_member}.nc'
-
-    templates = [filename_template, gridspec_template]
-    fileparts = reverse_filename_format(file_basename, templates)
-    frequency = extract_attr_with_regex(file, regex=freq_regex, strip_chars='/')
-    realm = extract_attr_with_regex(file, regex=realm_regex)
-    version = extract_attr_with_regex(file, regex=version_regex) or 'v0'
-    fileparts['frequency'] = frequency
-    fileparts['modeling_realm'] = realm
-    fileparts['version'] = version
-    fileparts['path'] = file
-    try:
-        part1, part2 = str(pathlib.Path(file).parent).split(fileparts['experiment'])
-        part1 = part1.strip('/').split('/')
-        fileparts['institute'] = part1[-2]
-        fileparts['product_id'] = part1[-3]
-    except Exception:
-        return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
+        return {INVALID_ASSET: filepath, TRACEBACK: traceback.format_exc()}
 
     return fileparts
