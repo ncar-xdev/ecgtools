8fcfd116cda33217a53277bf728e38176f3adf4b
mgrover1 <mgroverwx@gmail.com>
2022-10-17 15:31:52 -0500

    ADD: Add cmip6/cmip5 directory based parsers

--- ecgtools/parsers/cmip.py
+++ ecgtools/parsers/cmip.py
@@ -1,10 +1,12 @@
+import pathlib
 import traceback
 
 import cf_xarray  # noqa
+import numpy as np
 import xarray as xr
 
 from ..builder import INVALID_ASSET, TRACEBACK
-from .utilities import extract_attr_with_regex
+from .utilities import extract_attr_with_regex, reverse_filename_format
 
 
 def parse_cmip6(file):
@@ -86,3 +88,96 @@ def parse_cmip6(file):
 
     except Exception:
         return {INVALID_ASSET: file, TRACEBACK: traceback.format_exc()}
+
+
+def parse_cmip6_using_directories(file):
+    """
+    Extract attributes of a file using information from CMI6 DRS.
+    References
+    CMIP6 DRS: http://goo.gl/v1drZl
+    Controlled Vocabularies (CVs) for use in CMIP6: https://github.com/WCRP-CMIP/CMIP6_CVs
+    Directory structure =
+    <mip_era>/
+        <activity_id>/
+            <institution_id>/
+                <source_id>/
+                    <experiment_id>/
+                        <member_id>/
+                            <table_id>/
+                                <variable_id>/
+                                    <grid_label>/
+                                        <version>
+    file name=<variable_id>_<table_id>_<source_id>_<experiment_id >_<member_id>_<grid_label>[_<time_range>].nc
+    For time-invariant fields, the last segment (time_range) above is omitted.
+    Example when there is no sub-experiment: tas_Amon_GFDL-CM4_historical_r1i1p1f1_gn_196001-199912.nc
+    Example with a sub-experiment: pr_day_CNRM-CM6-1_dcppA-hindcast_s1960-r2i1p1f1_gn_198001-198412.nc
+    """
+    basename = pathlib.Path(file).name
+    filename_template = '{variable_id}_{table_id}_{source_id}_{experiment_id}_{member_id}_{grid_label}_{time_range}.nc'
+
+    gridspec_template = (
+        '{variable_id}_{table_id}_{source_id}_{experiment_id}_{member_id}_{grid_label}.nc'
+    )
+    templates = [filename_template, gridspec_template]
+    fileparts = reverse_filename_format(basename, templates=templates)
+    try:
+        parent = pathlib.Path(file).parent
+        parent_split = parent.split(f"/{fileparts['source_id']}/")
+        part_1 = parent_split[0].strip('/').split('/')
+        grid_label = parent.split(f"/{fileparts['variable_id']}/")[1].strip('/').split('/')[0]
+        fileparts['grid_label'] = grid_label
+        fileparts['activity_id'] = part_1[-2]
+        fileparts['institution_id'] = part_1[-1]
+        version_regex = r'v\d{4}\d{2}\d{2}|v\d{1}'
+        version = extract_attr_with_regex(parent, regex=version_regex) or 'v0'
+        fileparts['version'] = version
+        fileparts['path'] = file
+        if fileparts['member_id'].startswith('s'):
+            fileparts['dcpp_init_year'] = float(fileparts['member_id'].split('-')[0][1:])
+            fileparts['member_id'] = fileparts['member_id'].split('-')[-1]
+        else:
+            fileparts['dcpp_init_year'] = np.nan
+
+    except Exception:
+        pass
+
+    return fileparts
+
+
+def cmip5_parser(file):
+    """Extract attributes of a file using information from CMIP5 DRS.
+    Notes
+    -----
+    Reference:
+    - CMIP5 DRS: https://pcmdi.llnl.gov/mips/cmip5/docs/cmip5_data_reference_syntax.pdf?id=27
+    """
+
+    freq_regex = r'/3hr/|/6hr/|/day/|/fx/|/mon/|/monClim/|/subhr/|/yr/'
+    realm_regex = r'aerosol|atmos|land|landIce|ocean|ocnBgchem|seaIce'
+    version_regex = r'v\d{4}\d{2}\d{2}|v\d{1}'
+
+    file_basename = pathlib.Path(file).name
+
+    filename_template = (
+        '{variable}_{mip_table}_{model}_{experiment}_{ensemble_member}_{temporal_subset}.nc'
+    )
+    gridspec_template = '{variable}_{mip_table}_{model}_{experiment}_{ensemble_member}.nc'
+
+    templates = [filename_template, gridspec_template]
+    fileparts = reverse_filename_format(file_basename, templates)
+    frequency = extract_attr_with_regex(file, regex=freq_regex, strip_chars='/')
+    realm = extract_attr_with_regex(file, regex=realm_regex)
+    version = extract_attr_with_regex(file, regex=version_regex) or 'v0'
+    fileparts['frequency'] = frequency
+    fileparts['modeling_realm'] = realm
+    fileparts['version'] = version
+    fileparts['path'] = file
+    try:
+        part1, part2 = pathlib.Path(file).parent.split(fileparts['experiment'])
+        part1 = part1.strip('/').split('/')
+        fileparts['institute'] = part1[-2]
+        fileparts['product_id'] = part1[-3]
+    except Exception:
+        pass
+
+    return fileparts
--- ecgtools/parsers/utilities.py
+++ ecgtools/parsers/utilities.py
@@ -1,4 +1,11 @@
+import itertools
+import pathlib
 import re
+import subprocess
+from functools import lru_cache
+
+import dask
+from intake.source.utils import reverse_format
 
 
 def extract_attr_with_regex(
@@ -16,3 +23,53 @@ def extract_attr_with_regex(
         return match
     else:
         return None
+
+
+def reverse_filename_format(filename, templates):
+    """
+    Uses intake's ``reverse_format`` utility to reverse the string method format.
+    Given format_string and resolved_string, find arguments
+    that would give format_string.format(arguments) == resolved_string
+    """
+    x = {}
+
+    for template in templates:
+        try:
+            x = reverse_format(template, filename)
+            if x:
+                break
+        except ValueError:
+            continue
+    if not x:
+        print(f'Failed to parse file: {filename} using patterns: {templates}')
+    return x
+
+
+@lru_cache(maxsize=None)
+def get_asset_list(root_path, depth=0, extension='*.nc'):
+    from dask.diagnostics import ProgressBar
+
+    root = pathlib.Path(root_path)
+    pattern = '*/' * (depth + 1)
+
+    dirs = [x for x in root.glob(pattern) if x.is_dir()]
+
+    @dask.delayed
+    def _file_dir_files(directory):
+        try:
+            cmd = ['find', '-L', directory.as_posix(), '-name', extension]
+            proc = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            output = proc.stdout.read().decode('utf-8').split()
+        except Exception:
+            output = []
+        return output
+
+    print('Getting list of assets...\n')
+    filelist = [_file_dir_files(directory) for directory in dirs]
+    # watch progress
+    with ProgressBar():
+        filelist = dask.compute(*filelist)
+
+    filelist = list(itertools.chain(*filelist))
+    print('\nDone...\n')
+    return filelist
