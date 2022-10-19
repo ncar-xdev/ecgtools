135fcb9b1bad2ca4512158fed35efb81b542cacf
Max Grover <mgroverwx@gmail.com>
2022-10-19 13:07:28 -0500

    ADD: Add cmip6/cmip5 directory based parsers (#134)
    Co-authored-by: Anderson Banihirwe <axbanihirwe@gmail.com>

--- ecgtools/parsers/utilities.py
+++ ecgtools/parsers/utilities.py
@@ -1,5 +1,7 @@
 import re
 
+from intake.source.utils import reverse_format
+
 
 def extract_attr_with_regex(
     input_str: str, regex: str, strip_chars: str = None, ignore_case: bool = True
@@ -16,3 +18,23 @@ def extract_attr_with_regex(
         return match
     else:
         return None
+
+
+def reverse_filename_format(filename, templates):
+    """
+    Uses intake's ``reverse_format`` utility to reverse the string method format.
+    Given format_string and resolved_string, find arguments
+    that would give format_string.format(arguments) == resolved_string
+    """
+    x = {}
+
+    for template in templates:
+        try:
+            x = reverse_format(template, filename)
+            if x:
+                break
+        except ValueError:
+            continue
+    if not x:
+        print(f'Failed to parse file: {filename} using patterns: {templates}')
+    return x
--- tests/parsers/test_cmip.py
+++ tests/parsers/test_cmip.py
@@ -1,6 +1,10 @@
 import pytest
 
-from ecgtools.parsers.cmip import parse_cmip6
+from ecgtools.parsers.cmip import (
+    parse_cmip5_using_directories,
+    parse_cmip6,
+    parse_cmip6_using_directories,
+)
 
 
 @pytest.mark.parametrize(
@@ -18,3 +22,36 @@ def test_parse_cmip6(sample_data_directory, file_path):
     assert entry['grid_label'] == 'gn'
     assert entry['table_id'] == 'Amon'
     assert entry['variable_id'] == 'tasmax'
+
+
+@pytest.mark.parametrize(
+    'file_path',
+    [
+        'cmip/CMIP6/CMIP/BCC/BCC-ESM1/piControl/r1i1p1f1/Amon/tasmax/gn/v20181214/tasmax/tasmax_Amon_BCC-ESM1_piControl_r1i1p1f1_gn_185001-230012.nc'
+    ],
+)
+def test_parse_cmip6_using_directories(sample_data_directory, file_path):
+    path = sample_data_directory / file_path
+    entry = parse_cmip6_using_directories(path)
+    assert {'activity_id', 'variable_id', 'table_id'}.issubset(set(list(entry.keys())))
+    assert entry['experiment_id'] == 'piControl'
+    assert entry['member_id'] == 'r1i1p1f1'
+    assert entry['grid_label'] == 'gn'
+    assert entry['table_id'] == 'Amon'
+    assert entry['variable_id'] == 'tasmax'
+
+
+@pytest.mark.parametrize(
+    'file_path',
+    [
+        'cmip/cmip5/output1/CCCma/CanESM2/esmHistorical/mon/ocnBgchem/Omon/r1i1p1/v20111027/fgco2/fgco2_Omon_CanESM2_esmHistorical_r1i1p1_185001-200512.nc'
+    ],
+)
+def test_parse_cmip5_using_directories(sample_data_directory, file_path):
+    path = sample_data_directory / file_path
+    entry = parse_cmip5_using_directories(str(path))
+    assert {'model', 'variable', 'mip_table'}.issubset(set(list(entry.keys())))
+    assert entry['experiment'] == 'esmHistorical'
+    assert entry['ensemble_member'] == 'r1i1p1'
+    assert entry['mip_table'] == 'Omon'
+    assert entry['variable'] == 'fgco2'
